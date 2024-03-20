e91858c79ccf3a845f4b36b128878dca8602e644
Andrei Shabaev <shabaevandrew@gmail.com>
2024-03-20 17:49:45 +0300

    Внес изменения согласно замечаниям после проверки

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,12 +3,13 @@ from datetime import datetime
 from selene import browser
 
 @pytest.fixture
-def page_open():
+def configuration():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://google.com'
-
+    yield
+    browser.quit()
 
 def current_time():
     now = datetime.now().time()
-    return (f'{now.hour}:{now.minute}')
+    return f'{now.hour}:{now.minute}'
